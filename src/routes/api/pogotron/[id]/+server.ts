11e3bd80c5ada4c73e2362c2a903d7fc88e08980
Bryant Young <orb.lucid@gmail.com>
2023-07-30 09:00:36 -0700

    Fixing build

--- src/routes/api/pogotron/[id]/+server.ts
+++ src/routes/api/pogotron/[id]/+server.ts
@@ -1,5 +1,5 @@
 import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
-import { addPogotronData, deletePogotronData } from '$db/pogotron';
+import { insertPogotronData, deletePogotronData } from '$db/pogotron';
 
 async function deleteHandler({ params }: RequestEvent): Promise<Response> {
     try {
@@ -15,7 +15,7 @@ export const DELETE: RequestHandler = deleteHandler;
 async function putHandler({ body }: RequestEvent): Promise<Response> {
     try {
         const data = JSON.parse(body as string);
-        return addPogotronData(data);
+        return insertPogotronData(data);
     } catch (error) {
         return new Response(JSON.stringify({ error: 'Something went wrong' }), { status: 500 });
     }
