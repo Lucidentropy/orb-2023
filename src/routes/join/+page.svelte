66fe8f6b58d645c32f2b07c09d34567d0a843470
Bryant Young <orb.lucid@gmail.com>
2023-07-30 22:18:53 -0700

    Damnit Swisgars, I forgots to adds the files again.

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Joining Orb</title>
+	<meta name="description" content="Joining Orb" />
+</svelte:head>
+
+<script>
+	const orbTag = "{-o-}";
+</script>
+
+<div class="text-column">
+    <h1>Joining Orb</h1>
+    <p>I'm planning on having a join form here in the future, but if you're interested and get to this before I finish this form, hit us up on discord!</p>
+</div>
---
+++ src/routes/wow/+page.svelte
@@ -0,0 +1,4 @@
+<div class="text-column">
+    <h1>World of Warcraft</h1>
+
+</div>
