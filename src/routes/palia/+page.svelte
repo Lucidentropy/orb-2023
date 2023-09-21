b1b7de7bd1c4e216b60567eeb301e7536126a5e5
Bryant Young <orb.lucid@gmail.com>
2023-09-21 15:18:59 -0700

    Adding palian clock first draft

--- src/routes/palia/+page.svelte
+++ src/routes/palia/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import PaliaClock from "./PaliaClock.svelte";
+</script>
+
 <svelte:head>
 	<title>Orb - Palia</title>
 	<meta name="description" content="The Orb community in the cozy-life-sim mmo, Palia." />
@@ -6,7 +10,24 @@
 <div class="text-column">
     <h1>Palia</h1>
 
+    <div class="palia-clock">
+        <PaliaClock />
+    </div>
+
     <p>NPC bookmarker is in development.</p>
 
     <a href="/palia/bugcollector">Bug Collector</a>
 </div>
+
+<style>
+    .text-column {
+        min-height:400px;
+        justify-content: normal;
+    }
+    .palia-clock {
+        position:absolute;
+        top:20px;
+        right:20px;
+        overflow:hidden;
+    }
+</style>
---
+++ src/routes/palia/PaliaClock.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let currentTime: string = '';
+  let rotationAngle: number = 0;
+  const testSpeedMultiplier = 1; // Set this to 1 for real-time, higher values will speed up the clock
+
+  onMount(() => {
+    const updateClock = () => {
+      const now = new Date();
+      const minutes = (now.getMinutes() + now.getSeconds() / 60) * testSpeedMultiplier;
+      const hour24 = Math.floor(minutes / 2.5);
+      const hour12 = hour24 % 12 || 12;
+      const amPm = hour24 >= 12 ? 'PM' : 'AM';
+      currentTime = `${hour12.toString().padStart(2, '0')}:${Math.floor((minutes % 2.5) * 24).toString().padStart(2, '0')} ${amPm}`;
+
+      const totalMinutesInDay = 24 * 60;
+      const elapsedMinutes = (hour24 * 60 + minutes) % totalMinutesInDay;
+      rotationAngle = (elapsedMinutes / totalMinutesInDay) * 360;
+    };
+
+    updateClock();
+    setInterval(updateClock, 1000);
+  });
+</script>
+
+<style lang="scss">
+    .palia-clock {
+        width: 250px;
+        position: relative;
+
+        .clock {
+            background: url('/images/palia-clock.png') no-repeat 50% 50%;
+            width: 216px;
+            height: 208px;
+            position: relative;
+            margin:0 auto;
+        }
+
+        .indicator {
+
+            width: 114px;
+            height: 46px;
+            transform-origin: 108px 104px;
+            transition: 1s linear all;
+
+            img {
+                transform:rotate(150deg);
+            }
+        }
+
+        .current-time {
+            background-color: #ccc;
+            color: #fff;
+            text-align: center;
+            border-radius: 30px;
+            padding: 10px;
+            width:70%;
+            margin:30px auto 0;
+        }
+    }
+
+</style>
+
+<div class="palia-clock">
+    <div class="clock">
+        <div class="indicator" style={`transform: rotate(${rotationAngle + 210}deg);`} >
+            <img src="/images/palia-clock-indicator.png"/>
+        </div>
+        
+    </div>
+    <div class="current-time">
+        {currentTime}
+    </div>
+</div>
