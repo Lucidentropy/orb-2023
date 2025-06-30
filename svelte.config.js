054f2d133f358f89ad13b51f35297b966ab99fac
orb.lucid@gmail.com <bryant@dellaport.dev>
2025-06-29 19:29:40 -0700

    test fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,22 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
+import adapter from '@sveltejs/adapter-node';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: 'build' // default; just be explicit
-		}),
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
 		alias: {
 			$lib: resolve(__dirname, 'src/lib/'),
 			$db: resolve(__dirname, 'src/db/'),
