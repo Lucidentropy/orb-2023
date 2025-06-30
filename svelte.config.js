c44d8e5ae294f49e01b1c75c93acbce920981b9c
orb.lucid@gmail.com <bryant@dellaport.dev>
2025-06-29 19:24:54 -0700

    test fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite-plugin-svelte';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
