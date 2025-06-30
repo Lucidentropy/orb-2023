30b5433808d1f1c9f7620733611ba6b583c2025a
orb.lucid@gmail.com <bryant@dellaport.dev>
2025-06-29 19:05:04 -0700

    Disabling non-working scripts

--- src/routes/api/discord/+server.ts
+++ src/routes/api/discord/+server.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
+// import axios from 'axios';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export async function GET() {
-    const guild = process.env.DISCORD_GUILD_ID;
-    const response = await axios.get(`https://discordapp.com/api/guilds/${guild}/widget.json`);
+// export async function GET() {
+//     const guild = process.env.DISCORD_GUILD_ID;
+//     const response = await axios.get(`https://discordapp.com/api/guilds/${guild}/widget.json`);
 
-    return new Response(JSON.stringify(response.data), { status: 200 });
-}
+//     return new Response(JSON.stringify(response.data), { status: 200 });
+// }
 
