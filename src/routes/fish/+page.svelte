525e675c461ef8867cc52e5acb8dd43537b14199
orb.lucid@gmail.com <bryant@dellaport.dev>
2026-07-02 11:22:42 -0700

    Adding super important fish page

---
+++ src/routes/fish/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>fish</title>
+    <meta name="description" content="fish" />
+</svelte:head>
+
+<h1 class="font-serif text-4xl mt-8 mb-2 border-1 text-white text-center">fish</h1>
+<img src="/images/fish2.jpg" class="mx-auto shadow-lg my-4" alt="fish" />
+<p class="text-white text-center italic mt-4">fish, behold.</p>
